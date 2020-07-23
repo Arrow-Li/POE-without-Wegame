27a182bf476ccc2cc5bff0a0d9afea44695f9e3e
Arrow Li <jeep3333@gmail.com>
2020-07-23 10:57:32 +0800

    Change Check function

--- poe.cpp
+++ poe.cpp
@@ -1,58 +1,53 @@
+#include "string"
+#include "time.h"
+#include "fstream"
 #include "windows.h"
 #include "tlhelp32.h"
-#include "iostream"
 
-bool findProcess(LPCTSTR name);
 bool killProcess(LPCTSTR name);
 
 int main(int argc, char const *argv[]) {
-	auto a = (int)ShellExecute(NULL, NULL, PCHAR("launcher.exe"), NULL, NULL, SW_NORMAL);
-	if (a < 32)
+	auto exec_code = (int)ShellExecute(NULL, NULL, PCHAR("launcher.exe"), NULL, NULL, SW_NORMAL);
+	if (exec_code < 32) //调用失败
 		return 0;
 	Sleep(2000);
-	//std::cout << "Start Scan!";
+
+	std::fstream log_file;
+
+	if (argc > 1) //入参有文件路径
+		log_file.open(argv[1], std::ios::in);
+	if (!log_file.is_open()) //文件是否打开
+		return 0;
+
+	std::string line;
+	log_file.seekg(0, std::ios::end);
+
+	// Start Scan
 	while (true) {
-		if (findProcess("PathOfExile_x64.exe")) {
-			//std::cout << "Find!";
+		if (log_file.peek() == EOF) //实时更新文件指针
+		{
+			if (clock() > 300000) {
+				log_file.close();
+				return 0;
+			}
+			log_file.clear();
 			Sleep(2000);
-			while (true) {
-				if (findProcess("GameLoader.exe"))
-					break;
-				Sleep(99);
-			}
-			Sleep(9999);
-			if (killProcess("tgp_daemon.exe")) {
-				killProcess("GameLoader.exe");
-				return 0;
-			}
-			else
-				return 0;
 		}
-		Sleep(99);
+
+		std::getline(log_file, line);
+
+		if (line.find("Connected to") != std::string::npos) //已连接服务器
+		{
+			log_file.close();
+			break;
+		}
 	}
-	while (true);
+	// End Scan
+
+	killProcess("tgp_daemon.exe");
+	killProcess("GameLoader.exe");
+
 	return 0;
-}
-
-
-bool findProcess(LPCTSTR name) {
-	bool flag = false;
-	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	PROCESSENTRY32 pe; pe.dwSize = sizeof(PROCESSENTRY32);
-	if (!Process32First(hSnapshot, &pe))
-		return false;
-	while (true) {
-		pe.dwSize = sizeof(PROCESSENTRY32);
-		if (Process32Next(hSnapshot, &pe) == FALSE)
-			break;
-		if (strcmp(pe.szExeFile, name) == 0) {
-			flag = true;
-			break;
-		}
-		Sleep(1);
-	}
-	CloseHandle(hSnapshot);
-	return flag;
 }
 
 
